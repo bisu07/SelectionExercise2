a8f145d9f3d7c8b136e36065ac5c7f2160ff9b78
bisu07 <g26.bishal@gmail.com>
2021-09-29 22:32:55 -0500

    this is initial commit

---
+++ SwitchExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SwitchExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter your favourite subject:");
+            string subject = "math";
+            switch(subject)
+            {
+                case "science":
+                    Console.WriteLine("scinece");
+                    break;
+                case "biology":
+                    Console.WriteLine("biology");
+                    break;
+                case "math":
+                    Console.WriteLine("math");
+                    break;
+                case "history":
+                    Console.WriteLine("history");
+                    break;
+                case "arts":
+                    Console.WriteLine("arts");
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+    }
+}
